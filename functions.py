e8a91bf185add0d148f272cbae6e7c5dfa98d536
devalc <deva4998@vandals.uidaho.edu>
2018-09-17 21:15:52 -0700

    working SWE

--- functions.py
+++ functions.py
@@ -14,7 +14,8 @@ def process_data(filepath):
     data = pd.read_csv(filepath, nrows = 6954,parse_dates=['Date'], skiprows=1,\
                        names= ['Date', 'SWE_obs','P_accum','T_max','T_min',\
                                'T_avg','P_incremental']).dropna()
-    P = data['P_accum'].values
+    data['Jday'] = data['Date'].apply(lambda x: x.strftime('%j'))
+    P = data['P_incremental'].values
     T_avg = data['T_avg'].values
     SWE_obs= data['SWE_obs'].values
     return data, P, T_avg, SWE_obs
@@ -66,7 +67,7 @@ def simSWE(Psnow, meltflux):
     cumSWE = np.zeros(Psnow.shape, dtype=np.float32)
     act_melt = np.zeros(meltflux.shape, dtype=np.float32)
     for t in range(1,Psnow.shape[0]):
-        swe_inc = cumSWE[t-1] + Psnow[t] - meltflux[t]
+        swe_inc = (cumSWE[t-1] + Psnow[t]) - meltflux[t]
         if swe_inc > 0.0:
             cumSWE[t] = swe_inc
             act_melt[t] = meltflux[t]
@@ -89,26 +90,6 @@ def P_in(Prain, meltflux):
     melt = np.where(np.subtract(swe, meltflux)>0.0, meltflux, swe)
     ppt_in = np.add(melt, Prain)
     return ppt_in
-
-
-def simET( wp, wc,pet, fc):
-    """
-    simulates evapotranspiration by taking in:    
-    wp: Wilting point
-    wc: Water content
-    pet: Potential evapotranspiration
-    fc: Field capacity
-    
-    throws out:
-    simulate ET
-    """
-    theta = 1.0
-    if wc < 0.8*fc and wc > wp:
-        theta = (0.8*fc - wc)/(0.8*fc-wp)
-    elif wc <= wp:
-        theta = 0.0
-    return(pet*theta)
-
 
 
 def md(sim, obs):
@@ -135,3 +116,95 @@ def rmse(sim, obs):
     return(np.nanmean(np.sqrt(np.square(np.subtract(sim, obs)))))
 
 
+
+def celsius2kelvin(celsius):
+    """
+    Convert temperature in degrees Celsius to degrees Kelvin.
+    :param celsius: Degrees Celsius
+    :return: Degrees Kelvin
+    :rtype: float
+    """
+    return celsius + 273.15
+
+
+def kelvin2celsius(kelvin):
+    """
+    Convert temperature in degrees Kelvin to degrees Celsius.
+    :param kelvin: Degrees Kelvin
+    :return: Degrees Celsius
+    :rtype: float
+    """
+    return kelvin - 273.15
+
+
+def deg2rad(deg):
+    """
+    Convert angular degrees to radians
+    :param degrees: Value in degrees to be converted.
+    :return: Value in radians
+    :rtype: float
+    """
+    return deg * (np.pi / 180.0)
+
+
+def rad2deg(rads):
+    """
+    Convert radians to angular degrees
+    :param radians: Value in radians to be converted.
+    :return: Value in angular degrees
+    :rtype: float
+    """
+    return rads * (180.0 / np.pi)
+
+
+
+
+#def solar_decination_and
+#
+#
+#def Ra(,dr,G_sc= 0.0820):
+#    """
+#    Ra in MJ/m2day / 2.45 = Ra in mm/day
+#    
+#    calculates:
+#    Ra: Extraterrestrial radiation mm/day
+#    """
+
+
+def RefET_Hargreaves(Tmax, Tmin, Ra):
+    """
+    Args:
+    Tmax : max daily air temperature degC
+    Tmin : min daily air temperature degC
+    Ra: Extraterrestrial radiation mm/day
+    
+    calculates:
+    Tmean : mean daily air temperature degC calculated as (Tmax+Tmin)/2
+    ETo: Reference ET mm/day
+    
+    """
+    
+    ETo = 0.0023*np.sqrt(Tmax-Tmin)*(((Tmax+Tmin)/2)+17.8)*Ra
+    return ETo
+
+
+
+def simET( wp, wc, ETo, fc):
+    """
+    simulates actual evapotranspiration based on wilting point, water content\
+    field capacity
+    Args:    
+    wp: Wilting point
+    wc: Water content
+    ETo: Potential evapotranspiration
+    fc: Field capacity
+    
+    throws out:
+    Act ET
+    """
+    theta = 1.0
+    if wc < 0.8*fc and wc > wp:
+        theta = (0.8*fc - wc)/(0.8*fc-wp)
+    elif wc <= wp:
+        theta = 0.0
+    return(ETo*theta)
--- mod_run_file.py
+++ mod_run_file.py
@@ -12,13 +12,16 @@ import matplotlib.pyplot as plt
 
 filepath= 'D:/Chinmay/Hydro_modeling_erin_fall_2018/snotel_623_pre_mgmt.csv'
 
+
+#optimized parameters
 k = 1.1456
 tbase = 4.77
 train = 2.22
 tsnow = 0.76
 
+
 data, P, T_avg, SWE_obs = process_data(filepath)
-meltflux = Snowmelt_DD_usace(1.1456, T_avg)
+meltflux = Snowmelt_DD_usace(k, T_avg)
 Ps, Pr = P_to_Snow_and_Rain(P,T_avg)
 simSWE,actmelt = simSWE(Ps,meltflux)
 P_in = np.add(Pr, actmelt)
